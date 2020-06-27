73bc4faa8986b34156d7688516aeca1314d1ed58
friesenkiwi <git@friesenkiwi.de>
2020-06-27 22:43:30 +0200

    mongodb: Update to cover complete tutorial

--- roles/uberspace_mongodb/templates/.mongorc.js
+++ roles/uberspace_mongodb/templates/.mongorc.js
@@ -1,1 +1,1 @@
-db = connect("mongodb://{{ mongodb_admin_user }}:{{ mongodb_admin_pwd }}@127.0.0.1:27017/admin")
+db = connect("mongodb://{{ mongodb_admin_user }}:{{ mongodb_admin_password }}@127.0.0.1:27017/admin")
