67d6e7d73f8124569195778e0616f556a962dc29
friesenkiwi <git@friesenkiwi.de>
2025-09-17 00:00:46 +0200

    Add mosquitto role

---
+++ roles/uberspace_mosquitto/files/owntracks_db.sql
@@ -0,0 +1,18 @@
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `creator_hash` varchar(100) DEFAULT NULL,
+  `username` varchar(255) NOT NULL,
+  `tid` varchar(255) DEFAULT NULL,
+  `pw` varchar(255) NOT NULL,
+  `super` int(11) NOT NULL DEFAULT 0,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `users_unique` (`username`)
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8mb3 COLLATE=utf8mb3_unicode_ci;
+
+CREATE TABLE `acls` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `username` varchar(255) NOT NULL,
+  `topic` varchar(255) NOT NULL,
+  `rw` int(11) NOT NULL DEFAULT 1,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8mb3 COLLATE=utf8mb3_unicode_ci;
