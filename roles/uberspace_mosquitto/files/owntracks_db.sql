5ff8477c88cb3156757512bacbe4db98ebe7fd00
friesenkiwi <git@friesenkiwi.de>
2025-09-17 15:24:29 +0200

    geogame: add upsert functionality

--- roles/uberspace_mosquitto/files/owntracks_db.sql
+++ roles/uberspace_mosquitto/files/owntracks_db.sql
@@ -14,5 +14,6 @@ CREATE TABLE  IF NOT EXISTS `acls` (
   `username` varchar(255) NOT NULL,
   `topic` varchar(255) NOT NULL,
   `rw` int(11) NOT NULL DEFAULT 1,
-  PRIMARY KEY (`id`)
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `username_topic_unique` (`topic`,`username`) USING BTREE
 ) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8mb3 COLLATE=utf8mb3_unicode_ci;
